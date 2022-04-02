675b8befcd56f66a7d7d38d66d122fe77c4be12f
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-04-02 18:31:23 +0100

    cors origins update

--- project/settings.py
+++ project/settings.py
@@ -88,11 +88,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://londoncentralfitnessclub.com",
+    "https://www.londonfitnessdjango.com",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://londoncentralfitnessclub.com",
+    "https://www.londonfitnessdjango.com",
 ]
 
 
