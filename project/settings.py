0048b1677c3a8dcea8575381608aaa6fbd9549fa
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-04-04 14:31:42 +0100

    allowed hosts and cors settings update

--- project/settings.py
+++ project/settings.py
@@ -91,11 +91,15 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://www.londoncentralfitnessclub.com",
+    "http://api.citybik.es",
 ]
+
+
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://www.londoncentralfitnessclub.com",
+    "http://api.citybik.es",
 ]
 
 
